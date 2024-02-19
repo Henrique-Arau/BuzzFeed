ede8d5dc8bcc6e0cc2fe8057198a909191896eec
Henrique-Arau <penielh43@gmail.com>
2024-02-19 16:07:17 -0300

    Calculando o resultado

--- projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
+++ projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -42,14 +42,32 @@ export class QuizzComponent implements OnInit {
 
   }
 
-  nextStep(){
+  async nextStep(){
     this.questionIndex+=1;
 
     if(this.questionMaxIndex > this.questionIndex){
       this.questionSelected = this.questions[this.questionIndex];
     }else{
+      const finalAnswer:string = await this.checkResult(this.answers)
       this.finished = true;
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof 
+      typeof quizz_questions.results]
     }
   }
+
+  async checkResult(answers:string[]){
+    const result = answers.reduce((previous, current, i, arr)=>{
+      if(
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+      ){
+        return previous
+      }else{
+        return current
+      }
+    })
+    return result
+  }
+
 
 }
